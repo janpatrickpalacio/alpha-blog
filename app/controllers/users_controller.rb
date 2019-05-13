ba82d2cdfecfad94ea125b087115d859876fa9fb
Jan Patrick Palacio <janpatrickpalacio@gmail.com>
2019-05-13 20:42:53 +0800

    Added edit update functionality and partial for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Alpha Blog, #{@user.username}!"
+      flash[:success] = "Welcome to Alpha Blog, #{@user.first_name}!"
       redirect_to articles_path
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :first_name, :last_name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_120339) do
+ActiveRecord::Schema.define(version: 2019_05_13_122534) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_05_13_120339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
